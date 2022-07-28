8f2eda4bda50064e72daf05c7cb3f6f8c50712ef
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-27 21:50:14 -0400

    siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,14 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `WebDev Portfolio`,
+    description: `Awesome WebDev Portfolio built with Gatsby and Strapi`,
+    titleTemplate: `%s | WebDev Portfolio`,
+    // url:`https:// siteweb deployed`,
+    twitterUsername: `@john_doe`,
+    image: `/mainImg.png`,
+  },
 
   plugins: [
     `gatsby-plugin-image`,
