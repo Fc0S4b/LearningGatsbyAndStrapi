b14bd921c1222ebb498e1a22c02ba763ced304cd
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-27 22:56:50 -0400

    Seo complete

--- src/pages/projects/{StrapiProjects.title}.js
+++ src/pages/projects/{StrapiProjects.title}.js
@@ -4,6 +4,11 @@ import Seo from "../../components/Seo"
 const ProjectTemplate = ({ pageContext: { title }, data }) => {
   return (
     <>
+      <Seo
+        title={data.strapiProjects.title.toUpperCase()}
+        descriptrion={data.strapiProjects.description}
+        image={data.strapiProjects.image.localFile.publicURL} //check with twitter card validator and the image url from deployed project
+      />
       <main className="project-template-page">
         <h2>{title}</h2>
         <p>{data.strapiProjects.description}</p>
